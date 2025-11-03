e13029a0ad223ad0b91c19707d39dded744a3e0d
cocvu99 <cocvu99@gmail.com>
2025-11-04 01:26:43 +0700

    Implement: A Client send multiple requests to the tcp-server

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
-# sabaody-kv
+## sabaody-kv
 Cache data is like bubbles - temporary, subject to bursting (deleting), but very important and effective. Written in golang
 
 
 # How to run code
 
+TCP-server
+
 ```go build main.go```
+
+Start Server:
+# Terminal 1
+```cd tcp-server```
+```go run main.go```
+
+# Terminal 2
+Using telnet
+```telnet localhost 3000```
--- tcp-server/main.go
+++ tcp-server/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"log"
 	"net"
+	"strings"
 	"time"
 )
 
@@ -14,10 +17,51 @@ func handleConnection(conn net.Conn) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	// process
-	time.Sleep(time.Second * 7)
+
+	// Create reader to read line-by-line
+	reader := bufio.NewReader(conn)
+
+	/*
+	Create a loop: 
+		- Re-use and maintain the connection between client and server
+		- Pass data from buffer (read) client and send to the server
+		- Handle all exception error cases: client closing connection, ... TODO 
+		- TODO: Check all exception error case and handle them
+	*/
+	for {
+		conn.SetReadDeadline(time.Now().Add(15 * time.Second))
+		
+		message, err := reader.ReadString('\n')
+
+		// Handle all exception error cases
+		if err != nil {
+			// Normal case: Client closes connection first
+			if err.Error() == "EOF" {
+				log.Printf("Client %s disconnected", conn.RemoteAddr())
+				return
+			}
+		}
+
+		message = strings.TrimSpace(message)
+
+		// process after 500ms
+		time.Sleep(500 * time.Millisecond)
+
+		// Create response message and its format
+		response := fmt.Sprintf("Echo: %s (at %s)\n",
+			message, 
+			time.Now().Format("15:04:05"))
+
+		_, err = conn.Write([]byte(response))
+		if err != nil {
+			log.Printf("Error writing to %s: %v", conn.RemoteAddr(), err)
+			return
+		}
+
+	}
 	// reply
 	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\nHello, world\r\n"))
+
 	conn.Close()
 }
 
@@ -26,6 +70,8 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	log.Println("TCP-server is started. Listening at port 3000")
+
 	for {
 		// conn == socket == dedicated communication channel
 		conn, err := listener.Accept()
